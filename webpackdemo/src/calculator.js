2e67ebb9433bf0c2a408deeca8115bf6cfe212a7
Cj370118568 <370118568@qq.com>
2022-09-21 14:53:59 +0800

    feat: module example

---
+++ webpackdemo/src/calculator.js
@@ -0,0 +1,6 @@
+export default {
+  name: "cj",
+  add: function (a, b) {
+    return a + b;
+  },
+};
--- webpackdemo/src/index.js
+++ webpackdemo/src/index.js
@@ -1,3 +1,7 @@
 import addContent from "./add-content.js";
+import calculator from "./calculator.js";
 document.write("My first Webpack app.<br/>");
 addContent();
+
+const sum = calculator.add(3, 4);
+console.log(sum);
